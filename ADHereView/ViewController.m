5a983155e315f199c5ecdb523eb028fd4493df72
LiSai <995881161@qq.com>
2016-12-08 15:14:09 +0800

    Initial Commit

---
+++ ADHereView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ADHereView
+//
+//  Created by 李赛 on 16/12/8.
+//  Copyright © 2016年 李赛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ADHereView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ADHereView
+//
+//  Created by 李赛 on 16/12/8.
+//  Copyright © 2016年 李赛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
